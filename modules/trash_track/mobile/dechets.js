74db6dcfeff9bc6ea08424f819c105c45f58d993
William Lachance <wrlach@gmail.com>
2011-02-19 17:29:31 -0500

    Accidentally forgot to include js to display add bookmark bubble

--- modules/trash_track/mobile/dechets.js
+++ modules/trash_track/mobile/dechets.js
@@ -1,3 +1,43 @@
+window.addEventListener('load', function() {
+    window.setTimeout(function() {
+	var bubble = new google.bookmarkbubble.Bubble();
+	
+	var parameter = 'bmb=1';
+	
+	bubble.hasHashParameter = function() {
+	    return window.location.hash.indexOf(parameter) != -1;
+	};
+	
+	bubble.setHashParameter = function() {
+	    if (!this.hasHashParameter()) {
+		window.location.hash += parameter;
+	    }
+	};
+	
+	bubble.getViewportHeight = function() {
+	    window.console.log('Example of how to override getViewportHeight.');
+	    return window.innerHeight;
+	};
+	
+	bubble.getViewportScrollY = function() {
+	    window.console.log('Example of how to override getViewportScrollY.');
+	    return window.pageYOffset;
+	};
+	
+	bubble.registerScrollHandler = function(handler) {
+	    window.console.log('Example of how to override registerScrollHandler.');
+	    window.addEventListener('scroll', handler, false);
+	};
+	
+	bubble.deregisterScrollHandler = function(handler) {
+	    window.console.log('Example of how to override deregisterScrollHandler.');
+	    window.removeEventListener('scroll', handler, false);
+	};
+	
+	bubble.showIfAllowed();
+    }, 1000);
+}, false);
+
 $(document).ready(function () {
     $('form#lookup_form').submit(function() {
         var inputData = { address: $('input#postal_code').val() };
