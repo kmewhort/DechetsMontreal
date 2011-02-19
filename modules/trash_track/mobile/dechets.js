fe924c5dcdeb3dc3ce02768d4d91d3684f54743f
William Lachance <wrlach@gmail.com>
2011-02-19 17:14:30 -0500

    Various improvements to the javascript front-end for the mobile website
    * Use a minified version of jQuery 1.5
    * Show loading status
    * Show an explicit error if we can't get or parse the json from the server

--- modules/trash_track/mobile/dechets.js
+++ modules/trash_track/mobile/dechets.js
@@ -1,33 +1,39 @@
 $(document).ready(function () {
-        $('form#lookup_form').submit(function() {
-            var inputData = { address: $('input#postal_code').val() };
-	    $.getJSON('/json', inputData, function(data) {
-	        if(data.status != 'SUCCESS') {
-	            $('div#map').html('');
-	            $('div#collection_times_content').html('<p class="address_error">' + data.status + '</p>');
-	        }
-		var latlng = data.geometry.location.lat + ',' + data.geometry.location.lng;
-		var mapHTML = '<img src=\"http://maps.google.com/maps/api/staticmap?center=' + latlng;
-		mapHTML += '&markers=' + latlng;
-		mapHTML += '&zoom=15&size=240x140&maptype=roadmap&sensor=false\"/>'
-		$('div#map').html(mapHTML);
-
-		var pickupsHTML = '<table>';
-		for (var i in data.collections) {
-		    var pickup = data.collections[i];
-		    pickupsHTML += '<tr>';
-		    pickupsHTML += '<td valign=\"top\">' + '<img src=\"' + pickup['icon'] + '\"/>';
-		    pickupsHTML += '<br/><b>' + pickup['type'] + '</b></td>'
-		    pickupsHTML += '<td>' + pickup['desc'] + '</td>';
-		    pickupsHTML += '</tr>';
-		}
-		pickupsHTML += '</table>';
-		$('div#collection_times_content').html(pickupsHTML);
-	    });
+    $('form#lookup_form').submit(function() {
+        var inputData = { address: $('input#postal_code').val() };
+	$('#submit_button').val('Working...');
+	$('#submit_button').css('color', "#aaa");
+	$('div#collection_times').hide();
+	$.getJSON('json', inputData, function(data) {
+	    $('#submit_button').val('Lookup');
+	    $('#submit_button').css('color', "#000");
+	    
+	    if(data.status != 'SUCCESS') {
+	            $('div#error').html('<p class="error">' + data.status + '</p>');
+		return false;
+	    }
+	    
+	    var latlng = data.geometry.location.lat + ',' + data.geometry.location.lng;
+	    var mapHTML = '<img src=\"http://maps.google.com/maps/api/staticmap?center=' + latlng;
+	    mapHTML += '&markers=' + latlng;
+	    mapHTML += '&zoom=15&size=240x140&maptype=roadmap&sensor=false\"/>'
+	    $('div#map').html(mapHTML);
+	    
+	    var pickupsHTML = '<table>';
+	    for (var i in data.collections) {
+		var pickup = data.collections[i];
+		pickupsHTML += '<tr>';
+		pickupsHTML += '<td valign=\"top\">' + '<img src=\"' + pickup['icon'] + '\"/>';
+		pickupsHTML += '<br/><b>' + pickup['type'] + '</b></td>'
+		pickupsHTML += '<td>' + pickup['desc'] + '</td>';
+		pickupsHTML += '</tr>';
+	    }
+	    pickupsHTML += '</table>';
+	    $('div#collection_times_content').html(pickupsHTML);
 	    $('div#intro').hide();
 	    $('div#collection_times').show();
 	    $('span#search_term').html($('input#postal_code').val());
-
+	    
 	    var weekDate = new Date();
 	    // if we're on saturday, get next week's schedule, otherwise get this week's
 	    if (weekDate.getDay() === 6) {
@@ -35,10 +41,20 @@ $(document).ready(function () {
 	    } else {
 		weekDate.setDate(weekDate.getDate() - weekDate.getDay());
 	    }
-
+	    
 	    $('span#search_week').html( weekDate.toLocaleDateString());
 	    
-
-	    return false;
+	    $('div#collection_times').show();
+	})
+	    .error(function() { 
+		$('div#error').html('<p class="error">Server error. Please try again later.</p>');
+		return false; 
+	    })
+	    .complete(function() { 
+		$('#submit_button').val('Lookup');
+		$('#submit_button').css('color', "#000");
+	    });
+	
+	return false;
     });
 });
